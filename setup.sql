3446721dfbd5535d60912ff8d8ce1bcaa4b94fae
Vasishth Gupta <76578694+vasishthg@users.noreply.github.com>
2024-08-09 00:35:48 +0530

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `core` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(415) DEFAULT NULL,
+  `password` varchar(415) DEFAULT NULL,
+  `arni` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
